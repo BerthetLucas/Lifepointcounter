04b5ab2978d5c5818fcbed82a726cedda4a50bfb
Lucas Berthet <lu.berthet@gmail.com>
2023-08-29 09:50:50 +0200

    add clear function

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let screen = document.getElementById("result")
+let screen = document.getElementById("result");
 
-
-function display (newvalue) {
-    screen.value = screen.value + newvalue;  
-    return newvalue; 
-};
-
-function solve(){
-
-    let acalculer = screen.value; 
-
-    let res = eval(screen.value); 
-
-    screen.value = res; 
-
-    return res; 
-
-    if (res === 0) {
-        let mess = document.querySelector(".message"); 
-        mess.innerHTML = "Vous avez perdu"; 
-    }else{
-        mess.innerHTML = "Le duel continu"; 
-    }
- 
+function display(newvalue) {
+  screen.value = screen.value + newvalue;
+  return newvalue;
 }
 
+function solve() {
+  let acalculer = screen.value;
+
+  let res = eval(screen.value);
+
+  screen.value = res;
+
+  return res;
+}
+
+function clearScreen() {
+document.getElementById('result').value = "";
+}
